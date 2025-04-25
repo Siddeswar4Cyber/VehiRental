233bf06d3db129155c6d15884542e7e6866ee323
Siddeswar-N <nimmalasiddhu6@gmail.com>
2025-04-25 15:21:47 +0530

    Updated files

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for,session,jsonify
 import MySQLdb
 import pandas as pd
-from datetime import datetime
+from datetime import datetime,timedelta
 from datetime import date
 import datetime
 import time
@@ -19,11 +19,11 @@ app = Flask(__name__)
 app.secret_key = "secret_key"
 
 
+
 @app.route('/admin/add-vehicle',methods=['GET', 'POST'])
 def add_vehicle():
     try:
         if request.method == 'POST':
-            print(request.form)
             reg_no = request.form['reg_no']
             type_id = request.form['type_id']
             location = request.form['location']
@@ -37,7 +37,6 @@ def add_vehicle():
             if result:
                 return render_template('add-vehicle.html', error_message="Vehicle already exists")
             try:
-                print(reg_no, type_id, location, status)
                 cursor.execute("INSERT INTO vehicles(reg_no, type_id, location, status) VALUES (%s, %s, %s, %s)", (reg_no, type_id, location, status))
                 db.commit()
                 return redirect(url_for('add_vehicle'))
@@ -74,7 +73,6 @@ def manage_vehicles():
 def delete_vehicle():
     if True:
         reg_no = request.args.get('reg_no')
-        print(reg_no)
         try:
             cursor = db.cursor()
             cursor.execute("DELETE FROM vehicles WHERE reg_no = %s", (reg_no,))
@@ -90,7 +88,6 @@ def maintenance():
         cursor.execute(f"""select m.maintenance_id,m.vehicle_reg,m.description,m.cost,m.maintenance_date
                             from maintenance m;""")
         result = cursor.fetchall()
-        print("***********")
         records = []
         for data in result:
             records.append(
@@ -102,7 +99,6 @@ def maintenance():
                'date' : data[4].strftime('%Y-%m-%d')  # format date to string
                 }
             )
-        print(records)
         cursor.execute(f"""select v.reg_no,vt.class,v.location,m.maintenance_date,v.next_maintenance_date
                             from vehicles v
                             join vehicle_types vt on v.type_id = vt.type_id
@@ -120,11 +116,9 @@ def maintenance():
                     'next_maintenance' : data[4].strftime('%Y-%m-%d') if data[4] else 'N/A'
                 }
             )
-        print(due_records)
     except Exception as e:
         return render_template('error.html',error_message=e)
     if request.method == 'POST':
-        print(request.form)
         if request.form['action'] == 'Edit_Record':
             maintenance_id = request.form['id']
             vehicle_reg_no = request.form['reg_no']
@@ -135,16 +129,13 @@ def maintenance():
             cursor = db.cursor()
             try:
                 cursor.execute("UPDATE maintenance SET vehicle_reg = %s, description = %s, cost = %s, maintenance_date = %s WHERE maintenance_id = %s", (vehicle_reg_no, description, cost, date, maintenance_id))
-                print("Update Record of maintenance")
                 cursor.execute("UPDATE vehicles SET next_maintenance_date = %s WHERE reg_no = %s", (next_date, vehicle_reg_no))
-                print("Update next_maintenance_date of vehicle")
                 db.commit()
                 return redirect(url_for('maintenance'))
             except Exception as e:
                 db.rollback()
                 return render_template('error.html',error_message=e)
         else:
-            print(request.form['action'])
             vehicle_reg_no = request.form['reg_no']
             description = request.form['description']
             cost = request.form['cost']
@@ -154,9 +145,7 @@ def maintenance():
             maintenance_id = 'MT'+str(datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")[2:])
             try:
                 cursor.execute("INSERT INTO maintenance(maintenance_id, vehicle_reg, description, cost, maintenance_date) VALUES (%s, %s,%s,%s,%s)", (maintenance_id, vehicle_reg_no, description, cost, date))
-                print("Insert Record of maintenance")
                 cursor.execute("UPDATE vehicles SET next_maintenance_date = %s WHERE reg_no = %s", (next_date, vehicle_reg_no))
-                print("Update next_maintenance_date of vehicle")
                 db.commit()
                 return redirect(url_for('maintenance'))
             except Exception as e:
@@ -167,10 +156,8 @@ def maintenance():
    
 @app.route('/admin/delete-maintenance',methods=['GET', 'POST'])
 def delete_maintenance():
-    print("Delete Maintenance")
     if True:
         maintenance_id = request.args.get('maintenance_id')
-        print(maintenance_id)
         cursor = db.cursor()
         try:
             cursor.execute("DELETE FROM maintenance WHERE maintenance_id = %s", (maintenance_id,))
@@ -180,6 +167,47 @@ def delete_maintenance():
             db.rollback()
             return render_template('error.html',error_message=e)
         
+        
+def format_timedelta_to_time(td):
+    total_seconds = int(td.total_seconds())
+    hours = total_seconds // 3600
+    minutes = (total_seconds % 3600) // 60
+    seconds = total_seconds % 60
+    return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+
+@app.route('/admin/all-bookings',methods=['GET', 'POST'])
+def all_bookings():
+    try:
+        cursor = db.cursor()
+        cursor.execute(f""" select b.booking_id,b.pickup_date,b.pickup_time,b.return_date,b.return_time,b.transaction_id,b.user_id,v.class_type,v.class,v.icon,v.features,t.amount,t.payment_date as 'booking date',t.payment_mode
+                            from bookings b
+                            join vehicle_types v on b.vehicle_id = v.type_id
+                            join payments t on b.transaction_id = t.transaction_id;""")
+        result = cursor.fetchall()
+        bookings = []
+        for data in result:
+            bookings.append(
+                {   
+                'booking_id': data[0],
+                'pickup_date': data[1].strftime('%Y-%m-%d'),  # format date to string
+                'pickup_time': format_timedelta_to_time(data[2]),
+               'return_date': data[3].strftime('%Y-%m-%d'),
+               'return_time': format_timedelta_to_time(data[4]),
+                'transaction_id': data[5],
+                'user_id': data[6],
+                'class_type': data[7],
+                'class': data[8],
+                'icon': data[9],
+                'features': data[10].split(','),
+                'amount_paid': data[11],
+                'payment_date': data[12].strftime('%Y-%m-%d'),  # format date to string
+                'payment_method': data[13]
+                }
+            )
+        return render_template('all_booking_details.html', bookings=bookings)
+    except Exception as e:
+        return render_template('error.html',error_message=e)
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return redirect(url_for('login'))
@@ -194,6 +222,8 @@ def login():
         result = cursor.fetchone()
         if result:
             session['user_id'] = result[0]
+            if email == 'siddeswar@gmail.com':
+                return redirect(url_for('add_vehicle'))
             return redirect(url_for('home'))
         else:
             return render_template('login.html', error_message="Invalid email or password")
@@ -201,10 +231,7 @@ def login():
 
 @app.route('/register',methods=['GET', 'POST'])
 def register():
-    print("Register")
-    print(request.form)
     if request.method == 'POST':
-        print(request.form)
         fname = request.form['fname']
         mname = request.form['mname']
         lname = request.form['lname']
@@ -227,10 +254,7 @@ def register():
 
 @app.route('/forgot-password',methods=['GET', 'POST'])
 def forgot_password():
-    print("Forgot Password")
-    print(request.form)
     if request.method == 'POST':
-        print(request.form)
         email = request.form['email']
         newPassword = request.form['newPassword']
         confirmPassword = request.form['confirmPassword']
@@ -258,7 +282,6 @@ def home():
                             JOIN vehicle_types v ON b.vehicle_id = v.type_id
                             WHERE user_id = '{session['user_id']}' LIMIT 3;""")
         result = cursor.fetchall()
-        print(result)
         recent_bookings = []
         today = date.today()
         for data in result:
@@ -270,7 +293,6 @@ def home():
                 'status' : 'Completed' if data[2] < today else 'Ongoing' if data[1] < today and data[2] > today else 'Upcoming'
                 }
             )
-        print(recent_bookings)
         return render_template('home.html',recent_bookings=recent_bookings)
     except Exception as e:
         return render_template('error.html',error_message=e)
@@ -299,14 +321,13 @@ def get_vehicles():
             }
         )
     return jsonify(vehicles)
+
 @app.route('/home/book-vehicle',methods=['GET', 'POST'])
 def book_vehicle():
-    print("Book Vehicle")
     try:
         cursor = db.cursor()
         cursor.execute("SELECT * FROM vehicle_types")
         types = cursor.fetchall()
-        print(types)
         vehicleClasses = {}
         for i in types:
             if i[1] not in vehicleClasses:
@@ -322,10 +343,8 @@ def book_vehicle():
                 )
     except Exception as e:
         return render_template('error.html',error_message=e)
-    print(request.method)
     if request.method == 'POST':   
         try: 
-            print(request.form)
             pickup_date = request.form['pickup-date']
             pickup_time = request.form['pickup-time']
             return_date = request.form['return-date']
@@ -336,44 +355,35 @@ def book_vehicle():
             result = True
             transaction_id = ""
             while result:
-                print("Inside While")
                 now = datetime.now()
                 formatted_time = now.strftime("%Y%m%d%H%M%S%f")[:]
                 transaction_id = "TR"+formatted_time
                 cursor = db.cursor()
                 cursor.execute("select transaction_id from payments where transaction_id = %s",(transaction_id,))
                 result = cursor.fetchone()
-                print(result)
             try:
-                print(transaction_id)
                 cursor = db.cursor()
                 cursor.execute("INSERT INTO payments(transaction_id,amount,payment_mode) VALUES (%s, %s,%s)", (transaction_id,amount,mode ))
                 db.commit()
-                print("Payment Inserted")
             except Exception as e:
                 db.rollback()
                 return render_template('error.html',error_message=e)
             result = True
             booking_id = ""
             while result:
-                print("Inside While")
                 now = datetime.now()
                 formatted_time = now.strftime("%Y%m%d%H%M%S%f")[:]
                 booking_id = "BK"+formatted_time
                 cursor = db.cursor()
                 cursor.execute("select booking_id from bookings where booking_id = %s",(booking_id,))
                 result = cursor.fetchone()
-                print(result)
             try:
-                print(booking_id)
                 cursor = db.cursor()
                 cursor.execute("INSERT INTO bookings(transaction_id, pickup_date, pickup_time, return_date, return_time, user_id,booking_id,vehicle_id) VALUES (%s, %s,%s, %s,%s, %s,%s,%s)", (transaction_id, pickup_date, pickup_time, return_date, return_time,session['user_id'],booking_id,id))
                 db.commit()
-                print("Payment Inserted")
             except Exception as e:
                 db.rollback()
                 return render_template('error.html',error_message=e)
-            print(pickup_date,pickup_time,return_date,return_time,id,transaction_id)
             return render_template('book-vehicle.html',vehicleClasses=vehicleClasses,booking_id = booking_id, transaction_id = transaction_id)
         except Exception as e:
             return render_template('error.html',error_message=e)
@@ -390,7 +400,6 @@ def booking_history():
                         where user_id='{session['user_id']}'""")
         result = cursor.fetchall()
         booking_history = []
-        print('Booking History')
         for data in result:
             booking_history.append(
                 {
@@ -408,7 +417,6 @@ def booking_history():
                 'features': data[11]
                 }
             )
-        print(booking_history)
         return render_template('booking_history.html',booking_history=booking_history)
     except Exception as e:
         return render_template('error.html',error_message=e)
@@ -416,7 +424,6 @@ def booking_history():
 
 @app.route('/home/feedback',methods=['GET', 'POST'])
 def feedback():
-    print(request.method)
     if request.method == 'POST':
         cursor = db.cursor()
         try:
@@ -492,7 +499,6 @@ def report():
     except Exception as e:
         return render_template('error.html',error_message=e)
 
-    print(request.method)
     if request.method == 'POST':
         now = datetime.now()
         report_id = "RP"+str(now.strftime("%Y%m%d%H%M%S%f")[:])
@@ -502,7 +508,6 @@ def report():
             cursor = db.cursor()
             cursor.execute("INSERT INTO report(report_id,report_type,report_text,user_id) VALUES (%s, %s,%s,%s)", (report_id,report_type,report_text,session['user_id']))
             db.commit()
-            print("Report Inserted")
             return redirect(url_for('report'))
         except Exception as e:
             db.rollback()
@@ -531,7 +536,6 @@ def profile():
         }
         if request.method == 'POST':
             action = request.form.get('action')
-            print(action)
             if action == 'update':    
                 fname = request.form['fname']
                 mname = request.form['mname']
@@ -546,13 +550,11 @@ def profile():
                     cursor = db.cursor()
                     cursor.execute("UPDATE users SET fname = %s, mname = %s, lname = %s, email = %s, street = %s, city = %s, postal_code = %s, id_type = %s, id_number = %s WHERE user_id = %s", (fname, mname, lname, email, street, city, postal_code, id_type, id_number, session['user_id']))
                     db.commit()
-                    print("Profile Updated")
                     return redirect(url_for('profile'))
                 except Exception as e:
                     db.rollback()
                     return render_template('error.html',error_message=e)
             elif action == 'update_password':
-                print(request.form)
                 old_password = request.form['old_password']
                 new_password = request.form['new_password']
                 confirm_password = request.form['confirm_password']
@@ -562,13 +564,11 @@ def profile():
                 else:
                     try:
                         cursor = db.cursor()
-                        print(session['user_id'])
                         cursor.execute("SELECT password FROM users WHERE user_id = %s", (session['user_id'],))
                         result = cursor.fetchone()
                         if result[0] == old_password:
                             cursor.execute("UPDATE users SET password = %s WHERE user_id = %s", (new_password, session['user_id']))
                             db.commit()
-                            print("Password Updated")
                             return redirect(url_for('profile'))
                         else:
                             return render_template('profile.html',result=user, error_message="Incorrect Password")
